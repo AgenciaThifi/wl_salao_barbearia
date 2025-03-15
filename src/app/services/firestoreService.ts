741e12b4fd60875c2805107f54b5959f95747a50
DouglasiOSDeveloper <douguerpw2@hotmail.com>
2025-03-15 10:18:18 -0300

    removido possível erro de tipagem ao puxar/enviar dados do firestore

--- src/app/services/firestoreService.ts
+++ src/app/services/firestoreService.ts
@@ -1,11 +1,16 @@
 import { db } from "../config/firebase";
 import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 
+export interface Agendamento {
+  id: string;
+  nome: string;
+  telefone: string;
+  horario: string;
+}
+
 // Função para criar um agendamento
-export const criarAgendamento = async (agendamento) => {
+export const criarAgendamento = async (agendamento: Omit<Agendamento, "id">) => {
   try {
-    // Log para verificação
-    console.log("Tentando criar agendamento:", agendamento);
     await addDoc(collection(db, "agendamentos"), agendamento);
     console.log("Agendamento criado com sucesso!");
   } catch (error) {
@@ -13,27 +18,29 @@ export const criarAgendamento = async (agendamento) => {
   }
 };
 
+
 // Função para obter os horários disponíveis
-export const obterHorariosDisponiveis = async (horariosPadrao) => {
+export const obterHorariosDisponiveis = async (horariosPadrao: string[]): Promise<string[]> => {
   try {
     const q = query(collection(db, "agendamentos"));
     const querySnapshot = await getDocs(q);
-    
-    const horariosOcupados = querySnapshot.docs.map(doc => doc.data().horario);
-    return horariosPadrao.filter(hora => !horariosOcupados.includes(hora));
+
+    const horariosOcupados: string[] = querySnapshot.docs.map(doc => doc.data().horario as string);
+    return horariosPadrao.filter((hora: string) => !horariosOcupados.includes(hora));
   } catch (error) {
     console.error("Erro ao obter horários disponíveis:", error);
     return horariosPadrao; // Retorna os horários padrões caso haja erro
   }
 };
 
+
 // Função para obter todos os agendamentos
-export async function obterAgendamentos() {
+export async function obterAgendamentos(): Promise<Agendamento[]> {
   try {
     const querySnapshot = await getDocs(collection(db, "agendamentos"));
-    return querySnapshot.docs.map(doc => ({
+    return querySnapshot.docs.map((doc) => ({
       id: doc.id,
-      ...doc.data()
+      ...(doc.data() as Omit<Agendamento, "id">),
     }));
   } catch (error) {
     console.error("Erro ao obter agendamentos:", error);
